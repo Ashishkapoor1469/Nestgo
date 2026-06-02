147b4c0368bb11f5e5c4b92901c4b152302a436b
Ashish <kapoorashish713@gmail.com>
2026-06-02 13:58:40 +0530

    GOOGLE CONSOLE FIX

--- website/src/app/sitemap.ts
+++ website/src/app/sitemap.ts
@@ -1,24 +1,13 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://nestgo.vercel.app",
       lastModified: new Date(),
-      changeFrequency: "weekly",
       priority: 1,
-    },
-    {
-      url: "https://nestgo.vercel.app/docs",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: "https://nestgo.vercel.app/blog",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.7,
     },
   ];
 }
